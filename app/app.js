0fe0b31952e8422f5174efc9aebecb2f815bfad1
Zamir Kanji <zamirkanji@comcast.net>
2021-12-24 00:53:46 -0800

    final update 12.23.21

--- app/app.js
+++ app/app.js
@@ -1,24 +1,12 @@
 let log = console.log;
 
 const player = () => {
-    // const counter = () => {
-    //     let count = 0;
-    //     return () => {
-    //         console.log(count);
-    //         count++;
-    //     }
-    // }
-    const xMarker = () => {
-        return "x";
-    };
-    const oMarker = () => {
-        return "o"
-    };
+    const xMarker = () => "x";
+    const oMarker = () => "o";
     const createXBtn = (BTN) => {
         const divEl = document.createElement('div');
         divEl.textContent = "X";
         divEl.classList.add('x-btn');
-        // divEl.textContent = "O"; 
         BTN.appendChild(divEl);
     }
     const createOBtn = (BTN) => {
@@ -28,40 +16,12 @@ const player = () => {
         BTN.appendChild(divEl);
     }
     return {
-        counter,
         xMarker,
         oMarker,
         createXBtn,
         createOBtn
     };
 };
-// const playerTwo = () => {
-//     const counter = () => {
-//         let count = 0;
-//         return () => {
-//             console.log(count);
-//             count++;
-//         }
-//     }
-//     const oMarker = () => {
-//         return "o"
-//     };
-//     const createOBtn = (BTN) => {
-//         const divEl = document.createElement('div');
-//         divEl.textContent = "O";
-//         divEl.classList.add('o-btn');
-//         BTN.appendChild(divEl);
-//     }
-//     return {
-//         counter,
-//         oMarker,
-//         createOBtn
-//     };
-// };
-
-// console.log(player());
-
-// player.prototype = Object.create(createXBtn.prototype);
 
 const gameBoard = (() => {
     const game = [];
@@ -76,45 +36,36 @@ const displayController = (() => {
     const user1 = player();
     const user2 = player(); 
 
-    // const playerCounter = user1.counter();
-    // const playerTwoCounter = user2.counter();
-
     const createX = (BTN) => {
             user1.createXBtn(BTN);
-            // playerCounter();
             gameArr.game.push(user1.xMarker());
     }
     const createO = (BTN) => {
             user2.createOBtn(BTN);
-            // playerTwoCounter();
             gameArr.game.push(user2.oMarker());
     }
-    
-    // const {xMarker, oMarker, createXBtn} = player(); //inheritance from player class
+    const gameLogic = (BTN) => {
+        if (BTN.classList.contains('x-btn')) {
+            return;
+        }
+        if (BTN.classList.contains('o-btn')) {
+            return;
+        }
+        if ((gameArr.game[gameArr.game.length - 1] === 'x')) {
+            createO(BTN);
+        }
+        if (gameArr.game.length === 0 || (gameArr.game[gameArr.game.length - 1] === 'o')) {
+            log('test');
+            createX(BTN);
+        }
+    }
 
     const btnListener = () => {
         const gameBtns = document.querySelectorAll('.game-btn');
         gameBtns.forEach(btn => {
             btn.addEventListener('click', (e) => {
                 const BTN = e.target;
-                if (BTN.classList.contains('x-btn')) {
-                    return;
-                }
-                if (BTN.classList.contains('o-btn')) {
-                    return;
-                }
-                if (gameArr.game.length === 0 || (gameArr.game[gameArr.game.length - 1] === 'o')) {
-                    log('test');
-                    createX(BTN);
-                    // user1.createXBtn(BTN);
-                    // playerCounter();
-                    // gameArr.game.push(user1.xMarker());
-                }
-                if ((gameArr.game[gameArr.game.length - 1] === 'x') || (gameArr.game.length >= 1)) {
-                    createO(BTN);
-                    // user2.createOBtn(BTN);
-                    // playerTwoCounter();
-                }
+                gameLogic(BTN);
             })
         })
     }
@@ -122,6 +73,8 @@ const displayController = (() => {
     return { 
         gameArr, 
         createX,
+        createO,
+        gameLogic,
         btnListener 
     }
 
