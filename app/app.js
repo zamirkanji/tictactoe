797e1de75e7cd8a375105630e70c85ddc4db54dc
Zamir Kanji <zamirkanji@comcast.net>
2021-12-26 15:00:59 -0800

    figured out how to alternate between player 1 and player2

--- app/app.js
+++ app/app.js
@@ -10,9 +10,64 @@ const gameBoard = (() => {
 	return { game, checkForWinner };
 })();
 
+
 const player = () => {
     const xMarker = () => "x";
     const oMarker = () => "o";
+
+    return {
+        xMarker,
+        oMarker
+    };
+};
+
+const gameLogic = () => {
+    log('test');
+    const user1 = player();
+    const user2 = player(); 
+
+    const gameArr = Object.assign({}, gameBoard);
+
+    const createX = (BTN) => {
+        displayController.createXBtn(BTN);
+        gameArr.game.push(user1.xMarker());
+        log(gameArr);
+    }
+
+    const createO = (BTN) => {
+        displayController.createOBtn(BTN);
+        gameArr.game.push(user2.oMarker());
+        log(gameArr);
+    }
+
+    const switchPlayers = (BTN) => {
+        if (BTN.classList.contains('x-btn')) {
+            return;
+        }
+        if (BTN.classList.contains('o-btn')) {
+            return;
+        }
+        if ((gameArr.game[gameArr.game.length - 1] === 'x')) {
+            createO(BTN);
+            return; //this was it******************
+        }
+        if (gameArr.game.length === 0 || (gameArr.game[gameArr.game.length - 1] === 'o')) {
+            // log('test');
+            createX(BTN);
+            return;
+        }
+    }
+
+    return {
+        gameArr, 
+        createX, 
+        createO,
+        switchPlayers
+    }
+}
+
+const displayController = (() => {  
+
     const createXBtn = (BTN) => {
         const divEl = document.createElement('div');
         divEl.textContent = "X";
@@ -25,69 +80,27 @@ const player = () => {
         divEl.classList.add('o-btn');
         BTN.appendChild(divEl);
     }
-    return {
-        xMarker,
-        oMarker,
-        createXBtn,
-        createOBtn
-    };
-};
 
-const displayController = (() => {  
-    const gameArr = Object.assign({}, gameBoard);
-    console.log(gameArr);
-    
-    const user1 = player();
-    const user2 = player(); 
-
-    const createX = (BTN) => {
-        user1.createXBtn(BTN);
-        gameArr.game.push(user1.xMarker());
-    }
-    const createO = (BTN) => {
-        user2.createOBtn(BTN);
-        gameArr.game.push(user2.oMarker());
-    }
-    const gameLogic = (BTN) => {
-        if (BTN.classList.contains('x-btn')) {
-            return;
-        }
-        if (BTN.classList.contains('o-btn')) {
-            return;
-        }
-        if ((gameArr.game[gameArr.game.length - 1] === 'x')) {
-            createO(BTN);
-        }
-        if (gameArr.game.length === 0 || (gameArr.game[gameArr.game.length - 1] === 'o')) {
-            // log('test');
-            createX(BTN);
-        }
-    }
-    
-    
     return { 
-        gameArr, 
-        createX,
-        createO,
-        gameLogic
-        // btnListener 
-    }
-    
+        createXBtn,
+        createOBtn,
+    }    
 })();
 
 
-
-
-const btnListener = () => {
-    const gameBtns = document.querySelectorAll('.game-btn');
-    gameBtns.forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            const BTN = e.target;
-            displayController.gameLogic(BTN);
+const btnListener = (() => {
+    const game = gameLogic(); //this is creating an object "game" by calling the function
+    // log(game);
+    const allBtns = ()=> {
+        const gameBtns = document.querySelectorAll('.game-btn');
+        gameBtns.forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                const BTN = e.target;
+                game.switchPlayers(BTN);
+            })
         })
-    })
-}
+    }
+    return {allBtns}
+})();
 
-btnListener();
-
-// displayController.btnListener();
+btnListener.allBtns();
