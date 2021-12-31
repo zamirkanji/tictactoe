a08b03ccf446dad84e2fee92f98dabdf2d451031
Zamir Kanji <zamirkanji@comcast.net>
2021-12-30 19:08:25 -0800

    commit 1 12.30.21

--- app/app.js
+++ app/app.js
@@ -1,107 +1,150 @@
 let log = console.log;
 
 const player = () => {
+	const xMarker = () => "x";
+	const oMarker = () => "o";
 
-    const xMarker = () => "x";
-    const oMarker = () => "o";
-
-    return {
-        xMarker,
-        oMarker
-    };
+	return {
+		xMarker,
+		oMarker
+	};
 };
 
-const displayController = (() => { 
+const displayController = (() => {
+	const createXBtn = (BTN) => {
+		const divEl = document.createElement("div");
+		divEl.textContent = "X";
+		divEl.classList.add("x-btn");
+		BTN.appendChild(divEl);
+	};
+	const createOBtn = (BTN) => {
+		const divEl = document.createElement("div");
+		divEl.textContent = "O";
+		divEl.classList.add("o-btn");
+		BTN.appendChild(divEl);
+	};
 
-    const createXBtn = (BTN) => {
-        const divEl = document.createElement('div');
-        divEl.textContent = "X";
-        divEl.classList.add('x-btn');
-        BTN.appendChild(divEl);
-    }
-    const createOBtn = (BTN) => {
-        const divEl = document.createElement('div');
-        divEl.textContent = "O";
-        divEl.classList.add('o-btn');
-        BTN.appendChild(divEl);
-    }
-
-    return { 
-        createXBtn,
-        createOBtn
-    }    
+	return {
+		createXBtn,
+		createOBtn
+	};
 })();
 
 const gameBoard = ((btnID) => {
-    const gb = {};
-    const returnMarker = [];
-    const game = [];
+	const gb = {};
+	const returnMarker = [];
+	const game = [];
+	const testArr = [];
 
-
-    const user1 = player();
-    const user2 = player();
-
-    const checkForWinner = () => {
-        log(gb);
-
-        const gameKeys = Object.keys(gb);
-        const gameValues = Object.values(gb);
-        const entries = Object.entries(gb);
-
-        log(entries);
-
-        // if (game)
-
-    }
-
-    const createX = (BTN, btnID) => {
-        displayController.createXBtn(BTN);
-        returnMarker.push(user1.xMarker());
-        game.push(btnID);
-        gb[btnID] = "x";
-        checkForWinner();
-    }
-
-    const createO = (BTN, btnID) => {
-        displayController.createOBtn(BTN);
-        returnMarker.push(user2.oMarker());
-        game.push(btnID);
-        gb[btnID] = "o";
-        checkForWinner();
-    }
-
-    const switchPlayers = (BTN, btnID) => {
-        if(game.includes(btnID)) {
-            return;
-        }
-        if (BTN.classList.contains('x-btn')) {
-            return;
-        }
-        if (BTN.classList.contains('o-btn')) {
-            return;
-        }
-        if ((returnMarker[returnMarker.length - 1] === 'x')) {
-            createO(BTN, btnID);
-            return; //this was it******************
-        }
-        if (returnMarker.length === 0 || (returnMarker[returnMarker.length - 1] === 'o')) {
-            createX(BTN,btnID);
-            return;
+    const loopThroughArr = () => {
+        for (const el of testArr) {
+            // log(el);
+            if (el.hasChildNodes()) {
+                log('true');
+            }
         }
     }
 
-    const allBtns = ()=> {
-        const gameBtns = document.querySelectorAll('.game-btn');
-        gameBtns.forEach(btn => {
-            btn.addEventListener('click', (e) => {
-                const BTN = e.target;
-                const btnID = String(BTN.getAttribute('id'));
-                switchPlayers(BTN, btnID);
-            })
-        })
-    }
+	const pushMarker = (() => {
+		const gameBtns = document.querySelectorAll(".game-btn");
+		for (const btn of gameBtns) {
+			// if (btn.textContent !== "") {
+			// 	testArr.push(btn.textContent);
+			// }
+            testArr.push(btn);
+        }
+		
+
+		log(testArr);
+        return;
+	})();
+
     
-	return {checkForWinner, switchPlayers, createX, createO, allBtns, game, gb, returnMarker};
+
+	const user1 = player();
+	const user2 = player();
+
+	const checkForWinner = (BTN, btnID) => {
+		const displayWinner = document.querySelector("#display-txt");
+		log(gb);
+		log(game);
+		log(btnID, BTN);
+		game.forEach((el) => {
+			if (btnID === "one" && btnID === "two" && btnID === "three") {
+				if (BTN.innerText === "x") {
+					return (displayWinner.textContent = "User 1 is the winner!");
+				}
+			}
+		});
+		const gameKeys = Object.keys(gb);
+		const gameValues = Object.values(gb);
+		const entries = Object.entries(gb);
+
+		log(entries);
+	};
+
+	const createX = (BTN, btnID) => {
+		displayController.createXBtn(BTN);
+		returnMarker.push(user1.xMarker());
+		game.push(btnID);
+		gb[btnID] = "x";
+		loopThroughArr();
+		checkForWinner(BTN, btnID);
+	};
+
+	const createO = (BTN, btnID) => {
+		displayController.createOBtn(BTN);
+		returnMarker.push(user2.oMarker());
+		game.push(btnID);
+		gb[btnID] = "o";
+		loopThroughArr();
+		checkForWinner(BTN, btnID);
+	};
+
+	const switchPlayers = (BTN, btnID) => {
+		if (game.includes(btnID)) {
+			return;
+		}
+		if (BTN.classList.contains("x-btn")) {
+			return;
+		}
+		if (BTN.classList.contains("o-btn")) {
+			return;
+		}
+		if (returnMarker[returnMarker.length - 1] === "x") {
+			createO(BTN, btnID);
+			return; //this was it******************
+		}
+		if (
+			returnMarker.length === 0 ||
+			returnMarker[returnMarker.length - 1] === "o"
+		) {
+			createX(BTN, btnID);
+			return;
+		}
+	};
+
+	const allBtns = () => {
+		const gameBtns = document.querySelectorAll(".game-btn");
+		gameBtns.forEach((btn) => {
+			btn.addEventListener("click", (e) => {
+				const BTN = e.target;
+				const btnID = String(BTN.getAttribute("id"));
+				switchPlayers(BTN, btnID);
+			});
+		});
+	};
+
+	return {
+		checkForWinner,
+		switchPlayers,
+		createX,
+		createO,
+		allBtns,
+		game,
+		gb,
+		returnMarker
+	};
 })();
 
 gameBoard.allBtns();
